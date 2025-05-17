68d25307762439fa28aa549c562f79a9094f1d21
ramkumarss <ramssss467@gmail.com>
2025-05-17 23:15:58 +0530

    handle error

--- src/app/leaflet/page.js
+++ src/app/leaflet/page.js
@@ -1,5 +1,14 @@
+'use client'
+import dynamic from 'next/dynamic';
 import React from 'react';
-import LeafletWrapper from './leaflet-wrapper/leaflet-wrapper';
+
+const LeafletWrapper = dynamic(
+  () => import('@/app/leaflet/leaflet-wrapper/leaflet-wrapper'),
+  {
+    ssr: false,
+    loading: () => <p>Loading map...</p>
+  }
+);
 
 const Leaflet = () => {
   return <LeafletWrapper></LeafletWrapper>;
